5f2363917886494824ddbacf855eb4810e3c5129
R0hanR0y <roy087679@gmail.com>
2025-04-01 13:04:52 +0530

    Backedn Structure

---
+++ backend/models/Document.js
@@ -0,0 +1,3 @@
+// Document Schema (Document.js)
+
+// Fields: title, description, file, status (Pending, Rejected, Returned, Approved), currentStep (Operator, Verifier, Approver), history (array of action history), user (the user who submitted the document), etc.
---
+++ backend/models/User.js
@@ -0,0 +1,5 @@
+// User Schema (User.js)
+// Fields: email, password, role (Operator, Verifier, Approver), createdAt, etc.
+// email:rs1644818@gmail.com
+// password:12345
+// role:operator
