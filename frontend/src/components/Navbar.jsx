60c768a402364b82ad9f4d9dbc260b4e9b2f1f11
CodingBoy16 <rs1644818@gmail.com>
2025-04-03 18:23:47 +0530

    Error Fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
-import Profile from "./pages/Profile";
-import Applications from "./pages/Applications";
-import Accept from "./pages/Accept";
-import Reject from "./pages/Reject";
+import Profile from "./Profile";
+import Applications from "./Applications";
+import Accept from "./Accept"
+import Reject from "./Reject";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,16 +17,6 @@ const Navbar = () => {
   return (
     <div className="flex h-screen">
       {/* Left Side: Content */}
-      <div className="flex-1 p-6 bg-gray-100">
-        <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/applications" element={<Applications />} />
-          <Route path="/accept" element={<Accept />} />
-          <Route path="/reject" element={<Reject />} />
-        </Routes>
-      </div>
-
-      {/* Right Side: Navbar Menu */}
       <div className="w-64 bg-blue-900 text-white p-5 flex flex-col items-end shadow-lg">
         <NavLink to="/profile" className="py-2 hover:text-gray-300">
           Profile
@@ -46,6 +36,17 @@ const Navbar = () => {
         >
           Logout
         </button>
+      </div>
+
+      {/* Right Side: Navbar Menu */}
+
+      <div className="flex-1 p-6 bg-gray-100">
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/applications" element={<Applications />} />
+          <Route path="/accept" element={<Accept />} />
+          <Route path="/reject" element={<Reject />} />
+        </Routes>
       </div>
     </div>
   );
